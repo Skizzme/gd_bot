312c86d7f649f8e9f47c3064e61414a97da20ae5
Skizzme <62726778+Skizzme@users.noreply.github.com>
2024-05-25 20:57:24 -0400

    network forward works

--- src/main.rs
+++ src/main.rs
@@ -9,113 +9,16 @@ use std::thread::sleep;
 use std::time::{Duration, Instant};
 use ocl::{Buffer, Device, Platform, ProQue, SpatialDims};
 use ocl::ffi::libc::rand;
+use crate::network::Network;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let args: Vec<&str> = args.iter().map(String::as_str).collect();
-
-    let layer_nodes: i32 = i32::from_str(args[1]).unwrap();
-    let in_size: i32 = i32::from_str(args[2]).unwrap();;
-    let local_x: i32 = i32::from_str(args[3]).unwrap();;
-    let local_y: i32 = i32::from_str(args[4]).unwrap();;
-
-    let src = read_to_string("src\\test_comp").unwrap();
-    let platform = Platform::default();
-    let device = Device::first(platform).unwrap();
-
-    println!("{:?}", device.max_wg_size());
-
-    let pro_que = ProQue::builder().src(src).build();
-    if pro_que.is_err() {
-        std::fs::write("compile_error.txt", pro_que.err().unwrap().to_string()).expect("failed to write error");
-        return
-    }
-    let mut t = Instant::now();
-    let pro_que = pro_que.unwrap();
-    let queue = pro_que.queue();
-    // Flattens layers
-    let mut layer_1_flat = vec![0f64; (layer_nodes * in_size) as usize];
-    for i in 0..layer_1_flat.len() {
-        layer_1_flat[i] = rand::random() as f64;
-    }
-
-    let mut inputs = vec![0f64; in_size as usize];
-    for i in 0..inputs.len() {
-        inputs[i] = rand::random() as f64;
-    }
-    println!("buf_create: {:?}", t.elapsed());
+    println!("creating network");
+    let network = Network::new(vec![1024, 1024*16, 1024*32, 32], None, None).unwrap();
+    println!("{} {}", network.weights().len(), network.biases().len());
     let st = Instant::now();
-    t = Instant::now();
+    let out = network.forward(vec![1.0; 1024], 16);
+    println!("{:?} {:?}", st.elapsed(), out);
+    loop {
 
-    let weights_1: Buffer<f64> = Buffer::builder()
-        .queue(queue.clone())
-        .len(layer_1_flat.len())
-        .build().expect("Failed to make input buffer");
-    let inputs_1: Buffer<f64> = Buffer::builder()
-        .queue(queue.clone())
-        .len(inputs.len())
-        .build().expect("Failed to make input buffer");
-
-    weights_1.write(layer_1_flat.as_slice()).enq().expect("Failed to enq buffer 1");
-    println!("layer_weight_write: {:?} {:?}", t.elapsed(), st.elapsed());
-    t = Instant::now();
-
-    inputs_1.write(inputs.as_slice()).enq().expect("Failed to enq buffer 2");
-    println!("layer_inputs_write: {:?} {:?}", t.elapsed(), st.elapsed());
-    t = Instant::now();
-
-    let out_buffer: Buffer<f64> = Buffer::builder()
-        .queue(queue.clone())
-        .len(layer_nodes)
-        .build().expect("Failed to make output buffer");
-
-    println!("output_buf_create: {:?} {:?}", t.elapsed(), st.elapsed());
-    t = Instant::now();
-    let mut kernel = pro_que
-        .kernel_builder("layer")
-        .arg(layer_nodes)
-        .arg(in_size)
-        .arg(&weights_1)
-        .arg(&inputs_1)
-        .arg(&out_buffer)
-        // .global_work_size((layer_nodes, in_size))
-        // .local_work_size((local_x, local_y))
-        .build()
-        .unwrap();
-
-    println!("kernel_create: {:?} {:?}", t.elapsed(), st.elapsed());
-    t = Instant::now();
-
-    unsafe {
-        kernel
-            .cmd()
-            .global_work_size((layer_nodes, in_size))
-            .local_work_size((local_x, local_y))
-            .enq()
-            .unwrap();
-    }
-
-    println!("gpu_exec_time: {:?} {:?}", t.elapsed(), st.elapsed());
-
-    let mut out = vec![0f64; layer_nodes as usize];
-    out_buffer.read(&mut out).enq().unwrap();
-    println!("gpu_read_time: {:?} {:?} {} {} {}", t.elapsed(), st.elapsed(), out.len(), out[1], out[out.len()-1]);
-    // println!("gpu_out: {:?}", out);
-    t = Instant::now();
-
-    let mut cpu_out = vec![0f64; layer_nodes as usize];
-    for i in 0..layer_nodes {
-        for j in 0..in_size {
-            let ind: usize = (in_size * i + j) as usize;
-            cpu_out[i as usize] += layer_1_flat[ind] * inputs[j as usize];
-        }
-    }
-    println!("cpu_calc_time: {:?} {:?} {} {} {}", t.elapsed(), st.elapsed(), cpu_out.len(), cpu_out[1], cpu_out[cpu_out.len()-1]);
-    // println!("cpu_out: {:?}", cpu_out);
-
-    if args.len() > 5 && args[5] == "print" {
-        for i in 0..out.len() {
-            println!("CPU: {}, GPU: {} ERR: {}", cpu_out[i], out[i], (cpu_out[i] - out[i]).abs());
-        }
     }
 }
--- src/network.rs
+++ src/network.rs
@@ -1,26 +1,29 @@
 use std::fs::read_to_string;
+use std::time::Instant;
 use ocl::{Buffer, ProQue};
 use rand::random;
 
+#[derive(Debug)]
 pub struct Network {
     // Layer weights are flat, not a matrix
     weights: Vec<f64>,
     biases: Vec<f64>,
-    // Each layer size. First is the number of inputs
-    layers: Vec<u32>,
+    // Each layer size. First item is the inputs to the network
+    // nodes, weights_offset, biases_offset
+    layers: Vec<(i32, i32, i32)>,
 
-    gpu_kernel: ProQue,
+    gpu_proque: ProQue,
     // A single buffer containing all the weights, so fewer calls to gpu
     // Values will be gotten based off of the layer sizes and offsets etc
     gpu_weights: Buffer<f64>,
     // Individual output buffers for each layer, so each layer can have different sizes
     // Inputs to next layer will be outputs of last layer
-    gpu_layer_out_bufs: Vec<Buffer<f64>>,
+    gpu_layer_bufs: Vec<Buffer<f64>>,
 }
 
 impl Network {
-    pub fn new(layers: Vec<u32>, init_weights: Option<Vec<f64>>, init_biases: Option<Vec<f64>>) -> Result<Self, String> {
-
+    pub fn new(layers: Vec<i32>, init_weights: Option<Vec<f64>>, init_biases: Option<Vec<f64>>) -> Result<Self, String> {
+        println!("Compiling kernel");
         let src = read_to_string("src\\test_comp").unwrap();
         let pro_que = ProQue::builder().src(src).build();
         if pro_que.is_err() {
@@ -30,10 +33,19 @@ impl Network {
         let pro_que = pro_que.unwrap();
         let queue = pro_que.queue();
 
+        println!("Initializing layers");
+        let mut network_layers = vec![];
         let mut layer_buffers = vec![];
         let mut network_weights = vec![];
         let mut network_biases = vec![];
         let mut layer_inputs = *layers.get(0).unwrap();
+        // Add input layer, but since it isn't a real layer it doesn't have weights or biases
+        layer_buffers.push(Buffer::builder()
+                               .queue(queue.clone())
+                               .len(layer_inputs)
+                               .build().expect("Failed to make input buffer"));
+        network_layers.push((layers[0], 0, 0));
+
         let mut weight_offset = 0;
         let mut bias_offset = 0;
         // First value would be the plain inputs, so start creating layers with sizes after
@@ -49,10 +61,12 @@ impl Network {
             // Init weights/biases
             for j in 0..layer_size {
                 for k in 0..layer_inputs {
-                    network_weights.push(random())
+                    network_weights.push(random::<f64>()-0.5f64)
                 }
-                network_biases.push(random());
+                network_biases.push(random::<f64>()-0.5f64);
             }
+
+            network_layers.push((layer_size, weight_offset, bias_offset));
 
             weight_offset += layer_size*layer_inputs;
             bias_offset += layer_size;
@@ -61,36 +75,92 @@ impl Network {
             layer_inputs = layer_size;
         }
 
+        println!("Storing network weights on GPU");
         let weight_buf: Buffer<f64> = Buffer::builder()
             .queue(queue.clone())
             .len(network_weights.len())
             .build().expect("Failed to make input buffer");
         // Write all layers' weights to a single buffer
 
+        weight_buf.write(network_weights.as_slice()).enq().expect("Failed to write network weights");
+
         let mut network = Network {
             weights: network_weights,
             biases: network_biases,
-            layers: layers.clone(),
-            gpu_kernel: pro_que,
+            layers: network_layers,
+            gpu_proque: pro_que,
             gpu_weights: weight_buf,
-            gpu_layer_out_bufs: layer_buffers,
+            gpu_layer_bufs: layer_buffers,
         };
 
         Ok(network)
     }
 
-    // pub fn forward(&self) -> Vec<f64> {
-    //
-    //     pro_que
-    //         .kernel_builder("layer")
-    //         .arg(layer_nodes)
-    //         .arg(in_size)
-    //         .arg(&weights_1)
-    //         .arg(&inputs_1)
-    //         .arg(&out_buffer)
-    //         // .global_work_size((layer_nodes, in_size))
-    //         // .local_work_size((local_x, local_y))
-    //         .build()
-    //         .unwrap();
-    // }
+    pub fn forward(&self, mut inputs: Vec<f64>, work_size: i32) -> Result<Vec<f64>, String> { // Will return proper error later
+        let mut layer_in_size = self.layers[0].0;
+
+        if inputs.len() != layer_in_size as usize {
+            return Err(format!("Input length is incorrect! Network input length is: {}", layer_in_size).to_string())
+        }
+
+        let mut st = Instant::now();
+        let mut buf = &self.gpu_layer_bufs[0];
+        buf.write(inputs.as_slice()).enq().expect("Failed to write network inputs");
+        println!("Initialized network input {:?}", st.elapsed());
+        st = Instant::now();
+        for i in 1..self.layers.len() {
+            let (layer_size, weights_offset, biases_offset) = self.layers[i];
+            println!("Forwarding layer: {:?}. size: {:?}, weight_offset: {:?}, bias_offset: {:?}", i, layer_size, weights_offset, biases_offset);
+            let layer_buf = &self.gpu_layer_bufs[i];
+            let biases = self.biases[biases_offset as usize..(biases_offset+layer_size) as usize].iter().as_slice();
+            layer_buf.write(biases).enq().expect("Failed to set layer input buffer");
+            println!("Initialized layer output {:?}", st.elapsed());
+            st = Instant::now();
+
+            let layer_kernel = self.gpu_proque
+                .kernel_builder("layer")
+                .arg(i as i32)
+                .arg(layer_in_size)
+                .arg(weights_offset)
+                .arg(&self.gpu_weights)
+                .arg(buf)
+                .arg(layer_buf)
+                .build()
+                .unwrap();
+            println!("Created layer kernel {:?}", st.elapsed());
+            st = Instant::now();
+
+            unsafe {
+                layer_kernel
+                    .cmd()
+                    .global_work_size((layer_size, layer_in_size))
+                    .local_work_size((16, 16))
+                    .enq()
+                    .expect("Failed to enqueue kernel");
+            }
+            println!("Enqueued layer kernel {:?}", st.elapsed());
+            st = Instant::now();
+
+            layer_in_size = self.layers[i].0;
+            buf = layer_buf;
+        }
+
+        let mut final_out = vec![0f64; self.layers.get(self.layers.len() - 1).unwrap().0 as usize];
+        buf.read(&mut final_out).enq().expect("Failed to read output of network");
+        println!("Reading network output {:?}", st.elapsed());
+        st = Instant::now();
+
+        Ok(final_out)
+    }
+
+
+    pub fn weights(&self) -> &Vec<f64> {
+        &self.weights
+    }
+    pub fn biases(&self) -> &Vec<f64> {
+        &self.biases
+    }
+    pub fn layers(&self) -> &Vec<(i32, i32, i32)> {
+        &self.layers
+    }
 }
